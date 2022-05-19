531b4235b496345081261280840288bcf4544fad
chengcheng <chengcheng@qcraft.ai>
2022-05-19 19:56:05 +0800

    add gen dag hash

--- src/sk/window_sk.rs
+++ src/sk/window_sk.rs
@@ -20,10 +20,11 @@ use wasm_bindgen_test::__rt::js_console_log;
 // }
 
 const RAW: u64 = 0x55;
+const JSON: u64 = 0x0200;
 
 // 生成cid hash
 #[allow(clippy::unnecessary_wraps)]
-pub(crate) fn gen_hash(_: &JsValue, args: &[JsValue], context: &mut Context) -> JsResult<JsValue> {
+pub(crate) fn gen_hash_raw(_: &JsValue, args: &[JsValue], context: &mut Context) -> JsResult<JsValue> {
   let text = args
     .get_or_undefined(0)
     .to_string(context)
@@ -31,6 +32,20 @@ pub(crate) fn gen_hash(_: &JsValue, args: &[JsValue], context: &mut Context) -> 
   let h = Code::Sha2_256.digest(text.as_bytes());
 
   let cid = Cid::new_v1(RAW, h);
+  let cid_string = cid.to_string();
+  Ok(JsValue::from(cid_string))
+}
+
+// 生成dag cid hash
+#[allow(clippy::unnecessary_wraps)]
+pub(crate) fn gen_hash_dag(_: &JsValue, args: &[JsValue], context: &mut Context) -> JsResult<JsValue> {
+  let text = args
+    .get_or_undefined(0)
+    .to_string(context)
+    .expect("gen_hash: get arg error");
+  let h = Code::Sha2_256.digest(text.as_bytes());
+
+  let cid = Cid::new_v1(JSON, h);
   let cid_string = cid.to_string();
   Ok(JsValue::from(cid_string))
 }
@@ -50,7 +65,8 @@ pub(crate) fn init_sk(context: &mut Context) -> Result<(), JsValue> {
   let attribute = Attribute::READONLY | Attribute::NON_ENUMERABLE | Attribute::PERMANENT;
   let obj = ObjectInitializer::new(context)
     .property("name", "cwjsr_sk", attribute)
-    .function(gen_hash, "genHash", 1)
+    .function(gen_hash_raw, "genRawHash", 1)
+    .function(gen_hash_dag, "genDagHash", 1)
     .function(log, "log", 1)
     .build()
     .conv::<JsValue>()
